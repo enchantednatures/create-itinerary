c450fcfc24dc9782c19b8674bc5d83c2689885c5
hcasten <hcasten@outlook.com>
2024-01-21 08:50:05 -0700

    feat(axum): switch to axum

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,149 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
-use env_logger::{Builder, Env};
-use std::{
-    env,
-    io::{Error, ErrorKind, Result},
-    num::ParseIntError,
-};
+#![warn(dead_code)]
+use anyhow::Context;
+use anyhow::Result;
 
-mod config;
-mod handler;
+use tower_http::trace::TraceLayer;
 
-#[actix_web::main]
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::Registry;
+
+use axum::error_handling::HandleErrorLayer;
+use axum::extract::MatchedPath;
+use axum::http::Method;
+use axum::http::StatusCode;
+use axum::response::Response;
+use axum::routing::get;
+use axum::Router;
+use hyper::body::Bytes;
+use hyper::{HeaderMap, Request};
+
+use std::time::Duration;
+use tokio::time::error::Elapsed;
+use tower::BoxError;
+use tower::ServiceBuilder;
+use tower_http::classify::ServerErrorsFailureClass;
+use tower_http::cors::{Any, CorsLayer};
+use tracing::{info_span, Span};
+
+use axum::response::IntoResponse;
+use axum::Json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub enum HealthStatusEnum {
+    Ok,
+    Error,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct HealthStatus {
+    status: HealthStatusEnum,
+}
+
+impl HealthStatus {
+    pub(crate) fn new() -> Self {
+        HealthStatus {
+            status: HealthStatusEnum::Ok,
+        }
+    }
+}
+
+pub async fn health_check() -> impl IntoResponse {
+    (StatusCode::OK, Json(HealthStatus::new()))
+}
+
+pub fn create_router() -> Router {
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(Any);
+
+    Router::new()
+        .route("/", get(health_check).post(health_check))
+        .route("/health_check", get(health_check).post(health_check))
+        .layer(
+            ServiceBuilder::new()
+                .layer(cors)
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {}", error),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(10))
+                .layer(
+                    TraceLayer::new_for_http()
+                        .make_span_with(|request: &Request<_>| {
+                            // Log the matched route's path (with placeholders not filled in).
+                            // Use request.uri() or OriginalUri if you want the real path.
+                            let matched_path = request
+                                .extensions()
+                                .get::<MatchedPath>()
+                                .map(MatchedPath::as_str);
+
+                            info_span!(
+                                "http_request",
+                                method = ?request.method(),
+                                matched_path,
+                                some_other_field = tracing::field::Empty,
+                            )
+                        })
+                        .on_request(|_request: &Request<_>, _span: &Span| {
+                            // You can use `_span.record("some_other_field", value)` in one of these
+                            // closures to attach a value to the initially empty field in the info_span
+                            // created above.
+                        })
+                        .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                            // ...
+                        })
+                        .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                            // ...
+                        })
+                        .on_eos(
+                            |_trailers: Option<&HeaderMap>,
+                             _stream_duration: Duration,
+                             _span: &Span| {
+                                // ...
+                            },
+                        )
+                        .on_failure(
+                            |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                                // ...
+                            },
+                        ),
+                )
+                .into_inner(),
+        )
+}
+
+#[tokio::main]
 async fn main() -> Result<()> {
-    Builder::from_env(Env::default().default_filter_or("info")).init();
+    let formatting_layer = BunyanFormattingLayer::new("create-itinerary".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(JsonStorageLayer)
+        .with(EnvFilter::new("info"))
+        .with(formatting_layer);
 
-    let port = match env::var("PORT") {
-        Ok(v) => v
-            .parse()
-            .map_err(|e: ParseIntError| Error::new(ErrorKind::Other, e.to_string()))?,
-        Err(_) => 8080,
-    };
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+    let app = create_router();
 
-    // Create the HTTP server
-    HttpServer::new(|| {
-        App::new()
-            .wrap(actix_web::middleware::Logger::default())
-            .configure(config::configure)
-            .route("/", web::get().to(handler::index))
-            .route("/", web::post().to(handler::index))
-            .route(
-                "/health/{_:(readiness|liveness)}",
-                web::get().to(HttpResponse::Ok),
-            )
-    })
-    .bind(("0.0.0.0", port))?
-    .workers(1)
-    .run()
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .context("failed to bind TcpListener")
+        .unwrap();
+
+    axum::serve(
+        listener,
+        app.layer(TraceLayer::new_for_http()).into_make_service(),
+    )
     .await
+    .unwrap();
+    Ok(())
 }
